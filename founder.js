a70b8801de13ea2fd9fcc7b74b3106e482778ee8
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2025-12-01 06:41:57 +0600

    Create founder.js

---
+++ founder.js
@@ -0,0 +1,62 @@
+const args = process.argv.slice(2)
+
+let USER = args[0]
+
+console.log('User: '+USER)
+
+let mConfig = null
+
+process.on('message', (data) => {
+    try {
+        let json = (typeof data === 'string') ? JSON.parse(data) : data
+        if (json.t == 1) {
+            mConfig = json
+        }
+    } catch (error) {}
+})
+
+
+foundLoginNumber()
+
+
+async function foundLoginNumber() {
+    let loopDelay = 500
+    let load = 0
+
+    while (true) {
+        if (mConfig) {
+            try {
+                let prev = mConfig.n
+                let size = mConfig.s
+                let key = mConfig.u
+
+                for (let i = 0; i < size; i++) {
+                    try {
+                        if (prev != mConfig.n) {
+                            i = 0
+                            size = mConfig.s
+                            prev = mConfig.n
+                        }
+
+                        let number = mConfig.n+i
+
+                        await delay(loopDelay)
+                    } catch (error) {
+                        break
+                    }
+                }
+
+                mConfig = null
+                process.send({ t: 5, s: 'controller_status', c:USER+'_cmd', d: { t:1, u:key, s:USER, f:0, r:0, c:0, o:0 } })
+            } catch (error) {}
+        } else {
+            await delay(1000)
+        }
+    }
+}
+
+function delay(time) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, time)
+    })
+}
